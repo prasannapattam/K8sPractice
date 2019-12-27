f47dcdbbf765bea17e5cca9ce449fcdcc7a4330f
Prasanna Pattam <pprasannak@hotmail.com>
2019-12-27 11:18:52 +0530

    Created two services

---
+++ Doctor/Controllers/DoctorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Doctor.Controllers
+{
+    [ApiController]
+    [Route("{controller=doctor}/{action=index}")]
+    public class DoctorController : ControllerBase
+    {
+        public string Index()
+        {
+            return "Regular Doctor";
+        }
+        public string GetDoctor()
+        {
+            return "Specialist Doctor";
+        }
+    }
+}
---
+++ Patient/Controllers/PatientController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Patient.Controllers
+{
+    [ApiController]
+    [Route("{controller=patient}/{action=index}")]
+    public class PatientController : ControllerBase
+    {
+        public string Index()
+        {
+            return "Regular Patient";    
+        }
+        public string GetPatient()
+        {
+            return "Sick Patient";
+        }
+    }
+}
